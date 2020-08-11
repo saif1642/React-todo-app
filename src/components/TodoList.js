c078c1589d23042f46cffda4ec8d27c843be934e
saifdev1642 <saifdev1642@gmail.com>
2020-08-12 02:29:37 +0600

    component setup

---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello From TodoList</h3>
+            </div>
+        )
+    }
+}
